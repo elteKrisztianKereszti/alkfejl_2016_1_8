e7485db06f2df8ae65f0649658a763b106414116
Kereszti Krisztian <fragelek@inf.elte.hu>
2016-10-19 08:26:49 +0200

    5. vége

---
+++ app/Http/Controllers/PostController.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class PostController {
+  * ppap(request, response) {
+    yield response.sendView('main');
+  }
+}
+
+module.exports = PostController
---
+++ app/Http/Controllers/RecipeController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const Database = use('Database')
+
+class RecipeController {
+  * index (request, response) {
+    const categories = yield Database.from('categories').select('*');
+    console.log(categories);
+    yield response.sendView('main', {
+      name: 'Adonis',
+      categories: categories, 
+    });
+  }
+}
+
+module.exports = RecipeController
